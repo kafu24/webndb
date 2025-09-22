af0d2ef84029c42d54ef755c22a4baeda4fcc5b7
250MHz <95242017+250MHz@users.noreply.github.com>
2025-09-22 02:42:27 -0700

    db-api: Remove boilerplate route handlers

--- apps/db-api/app/__init__.py
+++ apps/db-api/app/__init__.py
@@ -1,27 +1,12 @@
 """Minimal Litestar application."""
 
-from asyncio import sleep
 from collections.abc import AsyncGenerator
-from typing import Any
 
-from litestar import Litestar, get
+from litestar import Litestar
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .database import async_engine, do_init_db
-
-
-@get('/')
-async def async_hello_world() -> dict[str, Any]:
-    """Route Handler that outputs hello world."""
-    await sleep(0.1)
-    return {'hello': 'world'}
-
-
-@get('/sync', sync_to_thread=False)
-def sync_hello_world() -> dict[str, Any]:
-    """Route Handler that outputs hello world."""
-    return {'hello': 'world'}
 
 
 async def provide_transaction(
@@ -35,7 +20,6 @@ def create_app() -> Litestar:
     alchemy_config = SQLAlchemyAsyncConfig(engine_instance=async_engine)
 
     return Litestar(
-        route_handlers=[sync_hello_world, async_hello_world],
         dependencies={'transaction': provide_transaction},
         plugins=[SQLAlchemyInitPlugin(alchemy_config)],
         on_startup=[do_init_db],
