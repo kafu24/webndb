55cb3262e9cb4788ab2dab509788621439ff620c
dphun123 <dennisphun321@gmail.com>
2025-09-26 01:37:11 -0700

    web: fixed sidebar trigger visibility delay

--- apps/web/src/components/nav/Sidebar/CollapsedSidebarTrigger.tsx
+++ apps/web/src/components/nav/Sidebar/CollapsedSidebarTrigger.tsx
@@ -1,9 +1,12 @@
 import { useSidebar, SidebarTrigger } from "@/components/ui/sidebar";
 
 export const CollapsedSidebarTrigger = () => {
-  const { state, toggleSidebar } = useSidebar();
-
-  if (state === "expanded") return null;
-
-  return <SidebarTrigger className="absolute top-4 left-2 z-50" />;
+  const { state } = useSidebar();
+  return (
+    <SidebarTrigger
+      className={`absolute top-4 left-2 z-50
+        ${state === "expanded" ? "invisible delay-0" : "visible delay-200"}
+        transition-visibility duration-0`}
+    />
+  );
 };
