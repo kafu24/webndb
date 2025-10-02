00e9088a7c4fef7ca39ae3ccd341e846d6391a72
dphun123 <dennisphun321@gmail.com>
2025-10-02 16:19:24 -0700

    web: astro add react

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,12 +5,14 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [preact()],
+  integrations: [preact(), react()],
   adapter: cloudflare(),
 });
