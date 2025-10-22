165beb167c10658b9454887a9c7671518247cb15
250MHz <95242017+250MHz@users.noreply.github.com>
2025-10-22 02:29:15 -0700

    db-api: Set up request and response body for query operations

--- apps/db-api/app/api/schemas.py
+++ apps/db-api/app/api/schemas.py
@@ -1,8 +1,12 @@
 import json
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Annotated, Any, Generic, TypeVar
 
 import msgspec
-from msgspec import Struct
+from litestar.openapi.spec.enums import OpenAPIType
+from litestar.params import Parameter
+from msgspec import Meta, Struct
+
+from app.config import DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE
 
 if TYPE_CHECKING:
     from typing import Any
@@ -65,3 +69,145 @@ class BaseStruct(Struct):
         return json.loads(
             msgspec.json.encode(self, enc_hook=bigint_enc_hook).decode('utf-8')
         )
+
+
+def create_q_param(searchable_fields: list[str] = None) -> Parameter:
+    return Parameter(
+        description=(
+            'Query string to set search terms. Search applies to'
+            f' {",".join([f"`{f}`" for f in searchable_fields])}.\n'
+            '\n'
+            'Note that if `q` is not an empty string, there will only'
+            ' be 1000 items to paginate through.'
+        )
+    )
+
+
+def create_filter_param(filterable_fields: list[str]) -> Parameter:
+    return Parameter(
+        description=(
+            'String with filter expressions to refine search results.'
+            ' Available fields are:'
+            f' {",".join([f"`{f}`" for f in filterable_fields])}.'
+        )
+    )
+
+
+def create_sort_pattern(sortable_fields: list[str]) -> str:
+    """`sort` in a `QueryRequest` child should have a type like:
+
+    `Annotated[list[Annotated[str, Meta(pattern=...)]], Parameter()]`
+
+    where `pattern` should be returned from this function.
+    """
+    return f'^({"|".join(sortable_fields)}):(asc|desc)$'
+
+
+def create_sort_parameter(
+    sortable_fields: list[str], default: list[str] = ['']
+) -> Parameter:
+    """`sort` in a `QueryRequest` child should have a type like:
+
+    `Annotated[list[Annotated[str, Meta(pattern=...)]], Parameter()]`
+
+    where `Parameter()` should be returned from this function.
+    """
+    return Parameter(
+        schema_extra={
+            'default': default,
+            'oneOf': None,
+            'type': OpenAPIType.ARRAY,
+            'items': {
+                'type': OpenAPIType.STRING,
+                'pattern': create_sort_pattern(sortable_fields),
+            },
+        },
+        description=(
+            'Sort results according to specified fields and indicated order.'
+            ' Note that if `q` is not empty, relevancy is prioritzed over sorting.'
+            ' The following fields are available for sorting:'
+            f' {",".join([f"`{f}`" for f in sortable_fields])}.'
+        ),
+        required=False,
+    )
+
+
+class QueryRequest(BaseStruct):
+    """Base container for storing query parameters for query operations."""
+
+    q: str = ''
+    offset: Annotated[
+        int,
+        Parameter(
+            ge=0,
+            description=(
+                'Sets the starting point in the search results'
+                ' (the number of resources to skip)'
+            ),
+            # ge=0 doesn't show in OpenAPI schema
+            schema_extra={'minimum': 0},
+        ),
+    ] = 0
+    limit: Annotated[
+        int,
+        Parameter(
+            ge=0,
+            description=(
+                'Sets the maximum number of documents returned by a request.'
+                f' Unset uses a defalut value of {DEFAULT_PAGE_SIZE}.'
+                f' Values above {MAX_PAGE_SIZE} will be coerced to {MAX_PAGE_SIZE}.'
+            ),
+            # ge=0 doesn't show in OpenAPI schema
+            schema_extra={'minimum': 0},
+        ),
+    ] = DEFAULT_PAGE_SIZE
+    fields: Annotated[
+        list[str] | None,
+        Parameter(
+            # TODO: consider setting max_items
+            schema_extra={
+                'default': ['*'],
+                'oneOf': None,
+                'type': OpenAPIType.ARRAY,
+                'items': {'type': OpenAPIType.STRING},
+            },
+            description=(
+                'Configures the fields that are returned in items.'
+                ' If `"*"` is received, then all fields are returned.'
+                ' Non-existent fields are ignored.'
+            ),
+            required=False,
+        ),
+    ] = None
+    filter: str = ''
+    sort: list[str] | None = None
+
+    def __post_init__(self):
+        self.limit = min(self.limit, MAX_PAGE_SIZE)
+        # We can't use msgspec.field(default_factory) (nor `[]` which is syntatic
+        # sugar for the same thing) as the default value for a field.
+        # It'll have the type `msgspec._core.Default` which raises a validation
+        # error since it doesn't match with the expected `list` type.
+        # Workaround is to use `None` as default value and change it to some list
+        # in __post_init__().
+        if self.fields is None:
+            self.fields = ['*']
+        if self.sort is None:
+            self.sort = []
+
+
+T = TypeVar('T')
+
+
+QueryItemsMeta = Meta(
+    title='Items', description='Array of objects representing the query results'
+)
+
+
+class QueryResponse(BaseStruct, Generic[T]):
+    """Container for data returned from a query."""
+
+    items: Annotated[list[T], QueryItemsMeta]
+    query: Annotated[str, Meta(description='Query originating the response')]
+    offset: Annotated[int, Meta(description='Number of resources skipped')]
+    limit: Annotated[int, Meta(description='Number of resources to take')]
