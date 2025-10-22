184b27587844e94da6dfe864080d005fd696717f
250MHz <95242017+250MHz@users.noreply.github.com>
2025-10-22 01:40:28 -0700

    db-api: models: Include table name in index naming convention

--- apps/db-api/app/models.py
+++ apps/db-api/app/models.py
@@ -19,7 +19,7 @@ class Language(StrEnum):
 class Base(DeclarativeBase):
     metadata = MetaData(
         naming_convention={
-            'ix': 'ix_%(column_0_N_label)s',
+            'ix': 'ix_%(table_name)s_%(column_0_N_label)s',
             'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
             'ck': 'ck_%(table_name)s_%(constraint_name)s',
             'fk': 'fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s',
