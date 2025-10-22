2e0d3a1ba963c02277285c4654a53ad5de6fe113
250MHz <95242017+250MHz@users.noreply.github.com>
2025-10-22 02:14:18 -0700

    db-api: Set up structlog

--- apps/db-api/app/__init__.py
+++ apps/db-api/app/__init__.py
@@ -2,9 +2,11 @@ from collections.abc import AsyncGenerator
 
 from litestar import Litestar
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
+from litestar.plugins.structlog import StructlogPlugin
 from litestar_granian import GranianPlugin
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .config import log_config
 from .database import async_engine, do_init_db
 
 
@@ -22,6 +24,7 @@ def create_app() -> Litestar:
         dependencies={'transaction': provide_transaction},
         plugins=[
             SQLAlchemyInitPlugin(alchemy_config),
+            StructlogPlugin(config=log_config),
             GranianPlugin(),
         ],
         on_startup=[do_init_db],
--- apps/db-api/app/database.py
+++ apps/db-api/app/database.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import structlog
 from alembic import command
 from alembic.config import Config
 from sqlalchemy import create_engine, inspect
@@ -8,6 +9,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from .config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_DATABASE_URI_SYNC
 from .models import Base
 
+logger = structlog.stdlib.get_logger()
 engine = create_engine(SQLALCHEMY_DATABASE_URI_SYNC)
 async_engine = create_async_engine(SQLALCHEMY_DATABASE_URI)
 
@@ -15,10 +17,16 @@ async_engine = create_async_engine(SQLALCHEMY_DATABASE_URI)
 def do_init_db():
     with engine.begin() as connection:
         if inspect(connection).has_table('alembic_versions'):
-            # TODO: log a message telling users to use `litestar database upgrade`
-            pass
+            logger.info(
+                '`alembic_versions` relation found.'
+                ' If needed, upgrade to the latest revision with'
+                ' `litestar database upgrade`.'
+            )
         else:
-            # TODO: log a message
+            logger.info(
+                '`alembic_versions` relation not found.'
+                ' Creating tables and stamping head.'
+            )
             Base.metadata.create_all(bind=connection)
             alembic_cfg = Config(
                 f'{pathlib.Path(__file__).parent.resolve().parent}/alembic.ini'
