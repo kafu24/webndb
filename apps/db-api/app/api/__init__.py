78a0747fa623f420993c88678dcb759910510c0d
250MHz <95242017+250MHz@users.noreply.github.com>
2025-12-07 11:47:49 -0800

    db-api: staff: Add query, read, create, update operations

--- apps/db-api/app/api/__init__.py
+++ apps/db-api/app/api/__init__.py
@@ -1,6 +1,7 @@
 from litestar import Router
 
 from .novel.views import novel_router
+from .staff.views import staff_router
 from .volume.views import volume_router
 
-api_router = Router('/', route_handlers=[novel_router, volume_router])
+api_router = Router('/', route_handlers=[novel_router, staff_router, volume_router])
---
+++ apps/db-api/app/api/staff/meili.py
@@ -0,0 +1,14 @@
+searchable_attributes = ['description', 'aliases.name', 'aliases.latin']
+filterable_attributes = [
+    'staff_id',
+    'staff_type',
+    'gender',
+    'primary_language',
+    'main_alias',
+    'description',
+    'languages.lang',
+    'aliases.name',
+    'aliases.latin',
+    'extlinks.link',
+]
+sortable_attributes = ['staff_id', 'main_alias', 'aliases.name', 'aliases.latin']
