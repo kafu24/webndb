05ff15a844ece77e047bdcbcc3152a6602ad123f
250MHz <95242017+250MHz@users.noreply.github.com>
2025-12-08 12:47:45 -0800

    db-api: Fix creating novel without staff

--- apps/db-api/app/api/staff/service.py
+++ apps/db-api/app/api/staff/service.py
@@ -44,6 +44,8 @@ async def select_nonexistent_staff_id(
     """Returns sequence of staff_id values that aren't associated with
     a staff record.
     """
+    if not staff_ids:
+        return []
     unnested = func.unnest(array(staff_ids)).column_valued('t')
     stmt = (
         select(unnested)
@@ -252,6 +254,8 @@ async def insert_staff_novel_by_novel_id(
     novel_id: str,
     staff_ids: Sequence[str],
 ) -> Sequence[StaffNovel]:
+    if not staff_ids:
+        return []
     try:
         stmt = insert(StaffNovel).values(
             [{'novel_id': int(novel_id), 'staff_id': int(id)} for id in staff_ids]
