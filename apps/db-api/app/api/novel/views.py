4e6bb24279ba83f84e6cdf0daf07cd4185581d1f
250MHz <95242017+250MHz@users.noreply.github.com>
2025-12-08 23:04:04 -0800

    db-api: Sort novel.titles descending
    TRUE > FALSE, so ascending would make non-official titles show up first.
    Easiest way to get titles to be sorted after inserting / updating seems
    to be to just select from database again, so I removed the code from
    to_novel_schema that accepts a titles sequence. Otherwise, we'd be
    storing records in the Meilisearch index with unofficial titles ordered
    earlier.

--- apps/db-api/app/api/novel/views.py
+++ apps/db-api/app/api/novel/views.py
@@ -289,12 +289,12 @@ async def create_novel(
         # No triggers on distributed tables, so we need to ensure a novel
         # has a volume_ordering record in application.
         await insert_initial_volume_ordering(transaction, novel.novel_id)
-        titles = await upsert_novel_titles(transaction, novel.novel_id, data.titles)
+        await upsert_novel_titles(transaction, novel.novel_id, data.titles)
         novel_staff = await upsert_novel_staff(
             transaction, novel.novel_id, data.staff, orm_staff
         )
         await insert_staff_novel_by_novel_id(transaction, novel.novel_id, staff_ids)
-        res = await to_novel_schema(novel, titles, novel_staff)
+        res = await to_novel_schema(novel, novel_staff)
         await meili_index.add_documents([res])
     except Exception:
         raise InternalServerException
@@ -418,11 +418,10 @@ async def patch_novel(
             if data.image_url is UNSET
             else data.image_url,
         )
-        titles = None
         novel_staff = None
         if data.titles is not UNSET:
             await clear_novel_titles(transaction, novel_id)
-            titles = await upsert_novel_titles(transaction, novel_id, data.titles)
+            await upsert_novel_titles(transaction, novel_id, data.titles)
         if data.staff is not UNSET:
             await clear_novel_staff(transaction, novel_id)
             await clear_staff_novel_by_novel_id(transaction, novel_id)
@@ -430,7 +429,7 @@ async def patch_novel(
                 transaction, novel.novel_id, data.staff, orm_staff
             )
             await insert_staff_novel_by_novel_id(transaction, novel.novel_id, staff_ids)
-        res = await to_novel_schema(novel, titles, novel_staff)
+        res = await to_novel_schema(novel, novel_staff)
         await meili_index.update_documents([res])
         await transaction.commit()
     except Exception:
